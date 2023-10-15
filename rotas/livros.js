3c009bfd93b0685aa64ee8faf36b108bed263baf
Gilmar <gilmar.jesus@gmail.com>
2023-10-15 16:46:55 -0300

    Criado acao para os verbos http post, patch e delete.

--- rotas/livros.js
+++ rotas/livros.js
@@ -6,4 +6,16 @@ router.get('/', (req, res)=>{
     res.send('Olá Mundo! Tudo bem?')
 })
 
+router.post('/', (req, res)=>{
+    res.send('Você fez uma requisição do tipo POST.')
+})
+
+router.patch('/', (req, res)=>{
+    res.send('Você fez uma requisição do tipo Patch.')
+})
+
+router.delete('/', (req, res)=>{
+    res.send('Você fez uma requisição do tipo Delete.')
+})
+
 module.exports = router
