bce41166b579b338f5ec060e7994aceadf9c992f
Gilmar <gilmar.jesus@gmail.com>
2023-10-15 17:02:04 -0300

    Criado controlador para separar a logica do arquivos de rotas.

--- rotas/livros.js
+++ rotas/livros.js
@@ -1,15 +1,7 @@
 const {Router} = require('express')
-
 const router = Router()
-
-router.get('/', (req, res)=>{
-    try{
-        res.send('Olá Mundo! Tudo bem?')
-    }catch (error){
-        res.status(500)
-        res.send(error.message)
-    }
-})
+const {getLivros} = require('../controladores/livros')
+router.get('/', getLivros)
 
 router.post('/', (req, res)=>{
     res.send('Você fez uma requisição do tipo POST.')
